2b9ed11a3ab0175c6ad595de06bced886d375435
Haekyu Lim <jackcong135@gamil.com>
2024-01-09 17:59:17 +0900

    history making

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import * as React from 'react';
 import GlobalStyle from './assets/styles/GlobalStyle.js'; // ! GlobalStyle.js : Reset css
 import Header from './component/essentials/Header.jsx';
 import Main from './component/essentials/Main.jsx';
+import { useEffect } from 'react';
 
 const App = () => {
+  // function setScreenSize() {
+  //   let vh = window.innerHeight * 0.01;
+  //   document.documentElement.style.setProperty('--vh', `${vh}px`);
+  // }
+
+  // useEffect(() => {
+  //   setScreenSize();
+  //   window.addEventListener('resize', setScreenSize);
+  //   return () => window.removeEventListener('resize', setScreenSize);
+  // }, []);
+
   return (
     <React.Fragment>
       <GlobalStyle />
--- src/component/contents/History.jsx
+++ src/component/contents/History.jsx
@@ -8,19 +8,119 @@ export default function History() {
           <div className="history-container-title">
             History<span className="blind">History</span>
           </div>
-          <div className="history-container-description">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-              distinctio numquam odio laborum adipisci maiores saepe tenetur
-              incidunt nihil provident voluptate in exercitationem eveniet
-              molestias, animi aspernatur nobis. Ut, nobis?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
-              blanditiis id dolor adipisci veritatis, possimus officiis amet
-              debitis temporibus, vitae, ducimus delectus. Nisi sequi fugit
-              harum delectus iusto ipsam ut?
-            </p>
+          <div className="history-item-list">
+            <div className="history-item left">
+              <div className="history-item-content">
+                <div className="history-year">2019</div>
+                <ul>
+                  <li className="history-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Natus distinctio numquam odio laborum adipisci maiores saepe
+                    tenetur incidunt nihil provident voluptate in exercitationem
+                    eveniet molestias, animi aspernatur nobis. Ut, nobis? Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Natus
+                    distinctio numquam odio laborum adipisci maiores saepe
+                    tenetur incidunt nihil provident voluptate in exercitationem
+                    eveniet molestias, animi aspernatur nobis. Ut, nobis?
+                  </li>
+                  <li className="history-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Natus distinctio numquam odio laborum adipisci maiores saepe
+                    tenetur incidunt nihil provident voluptate in exercitationem
+                    eveniet molestias, animi aspernatur nobis. Ut, nobis? Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Natus
+                    distinctio numquam odio laborum adipisci maiores saepe
+                    tenetur incidunt nihil provident voluptate in exercitationem
+                    eveniet molestias, animi aspernatur nobis. Ut, nobis?
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="history-item right">
+              <div className="history-item-content">
+                <div className="history-year">2020</div>
+                <ul>
+                  <li className="history-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Natus distinctio numquam odio laborum adipisci maiores saepe
+                    tenetur incidunt nihil provident voluptate in exercitationem
+                    eveniet molestias, animi aspernatur nobis. Ut, nobis? Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Natus
+                    distinctio numquam odio laborum adipisci maiores saepe
+                    tenetur incidunt nihil provident voluptate in exercitationem
+                    eveniet molestias, animi aspernatur nobis. Ut, nobis?
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="history-item left">
+              <div className="history-item-content">
+                <div className="history-year">2021</div>
+                <ul>
+                  <li className="history-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Natus distinctio numquam odio laborum adipisci maiores saepe
+                    tenetur incidunt nihil provident voluptate in exercitationem
+                    eveniet molestias, animi aspernatur nobis. Ut, nobis? Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Natus
+                    distinctio numquam odio laborum adipisci maiores saepe
+                    tenetur incidunt nihil provident voluptate in exercitationem
+                    eveniet molestias, animi aspernatur nobis. Ut, nobis?
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="history-item right">
+              <div className="history-item-content">
+                <div className="history-year">2022</div>
+                <ul>
+                  <li className="history-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Natus distinctio numquam odio laborum adipisci maiores saepe
+                    tenetur incidunt nihil provident voluptate in exercitationem
+                    eveniet molestias, animi aspernatur nobis. Ut, nobis? Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Natus
+                    distinctio numquam odio laborum adipisci maiores saepe
+                    tenetur incidunt nihil provident voluptate in exercitationem
+                    eveniet molestias, animi aspernatur nobis. Ut, nobis?
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="history-item left">
+              <div className="history-item-content">
+                <div className="history-year">2023</div>
+                <ul>
+                  <li className="history-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Natus distinctio numquam odio laborum adipisci maiores saepe
+                    tenetur incidunt nihil provident voluptate in exercitationem
+                    eveniet molestias, animi aspernatur nobis. Ut, nobis? Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Natus
+                    distinctio numquam odio laborum adipisci maiores saepe
+                    tenetur incidunt nihil provident voluptate in exercitationem
+                    eveniet molestias, animi aspernatur nobis. Ut, nobis?
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="history-item right">
+              <div className="history-item-content">
+                <div className="history-year">2024</div>
+                <ul>
+                  <li className="history-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Natus distinctio numquam odio laborum adipisci maiores saepe
+                    tenetur incidunt nihil provident voluptate in exercitationem
+                    eveniet molestias, animi aspernatur nobis. Ut, nobis? Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Natus
+                    distinctio numquam odio laborum adipisci maiores saepe
+                    tenetur incidunt nihil provident voluptate in exercitationem
+                    eveniet molestias, animi aspernatur nobis. Ut, nobis?
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </section>
